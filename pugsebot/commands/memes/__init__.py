157e6c5bda063563a21749f961bdd0d6754230f9
Carlson Santana Cruz <carlsonsantana@gmail.com>
2020-07-18 15:56:30 -0300

    Separar do módulo de ação do bot dos memes as funções de recuperar os memes

--- pugsebot/commands/memes/__init__.py
+++ pugsebot/commands/memes/__init__.py
@@ -1,63 +1,15 @@
 import random
+import os
 
 from utils.command_base import CommandBase
-from utils.request import get_html_soup
 from utils.time import UM_DIA_EM_SEGUNDOS
+from utils.module import get_modules_by_path
 
 CACHE_EXPIRES = 1800
 
-BASE_URL_VIDA_PROGRAMADOR = 'https://vidadeprogramador.com.br/'
-URL_VIDA_PROGRAMADOR_RANDOM = BASE_URL_VIDA_PROGRAMADOR + '+rand'
-URL_VIDA_PROGRAMADOR_TAG_TIRINHA = (
-    BASE_URL_VIDA_PROGRAMADOR
-    + 'tag-tirinhas.html'
-)
-URL_VIDA_PROGRAMADOR_TAG_REAL = (
-    BASE_URL_VIDA_PROGRAMADOR
-    + 'tag-real.html'
-)
-URL_VIDA_PROGRAMADOR_TAG_USUARIO = (
-    BASE_URL_VIDA_PROGRAMADOR
-    + 'tag-usuario.html'
-)
-URLS_VERIFICAR_VIDA_PROGRAMADOR = [
-    URL_VIDA_PROGRAMADOR_TAG_TIRINHA,
-    URL_VIDA_PROGRAMADOR_TAG_REAL,
-    URL_VIDA_PROGRAMADOR_TAG_USUARIO,
-]
-
-def get_url_image_vida_programador():
-    meme_found = False
-
-    while not meme_found:
-        document = get_html_soup(URL_VIDA_PROGRAMADOR_RANDOM)
-        for url in URLS_VERIFICAR_VIDA_PROGRAMADOR:
-            tags_found = bool(document.select(
-                '.postmetadata > a[href="{}"]'.format(url)
-            ))
-            if tags_found:
-                break
-
-        metadata_image = document.select('article.post a img')
-        meme_found = tags_found and bool(metadata_image)
-
-    return metadata_image[0]['src']
-
-BASE_URL_TURNOFF_US = 'https://turnoff.us'
-URL_TURNOFF_US_ALL_POSTS = BASE_URL_TURNOFF_US + '/pt/all/'
-
-def get_url_image_turnoff_us():
-    document = get_html_soup(URL_TURNOFF_US_ALL_POSTS)
-    random_link = random.choice(document.select('.post-link'))
-    random_url = random_link['href']
-
-    random_document = get_html_soup(BASE_URL_TURNOFF_US + random_url)
-    random_image = random_document.select('.post-content img')
-    return BASE_URL_TURNOFF_US + random_image[0]['src']
-
+MEMES_MODULES = get_modules_by_path(os.path.dirname(os.path.abspath(__file__)))
 MEMES_IMAGES_FUNCTIONS = [
-    get_url_image_vida_programador,
-    get_url_image_turnoff_us,
+    meme_module.get_meme_url_image for meme_module in MEMES_MODULES
 ]
 
 class Memes(CommandBase):
